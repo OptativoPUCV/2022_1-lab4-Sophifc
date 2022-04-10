63fd9dfb72644a0f1c2eedcbfc837b4e6d257f59
Sophifc <>
2022-04-10 23:30:52 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -120,7 +120,7 @@ long pos=hash(key, map->capacity);
 Pair * firstMap(HashMap * map) {
   
   for(long i=0; i<map->size; i++){
-    if(map->buckets[i]==NULL && map->buckets[i]->key!=NULL){
+    if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL){
       map->current=i;
       return map->buckets[i];
     }
